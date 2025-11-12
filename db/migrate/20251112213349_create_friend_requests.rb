edfdf2202d971db600bbcdea3c1b508e8971f6c2
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-11-12 14:36:13 -0700

    feat: add constraints & index to migration

--- db/migrate/20251112213349_create_friend_requests.rb
+++ db/migrate/20251112213349_create_friend_requests.rb
@@ -1,11 +1,13 @@
 class CreateFriendRequests < ActiveRecord::Migration[8.0]
   def change
     create_table :friend_requests do |t|
-      t.references :sender, null: false, foreign_key: true
-      t.references :receiver, null: false, foreign_key: true
-      t.string :status
+      t.references :sender, null: false, foreign_key: { to_table: :users }
+      t.references :receiver, null: false, foreign_key: { to_table: :users }
+      t.string :status, default: 'pending', null: false
 
       t.timestamps
     end
+
+    add_index :friend_requests, [:sender_id, :receiver_id], unique: true
   end
 end
