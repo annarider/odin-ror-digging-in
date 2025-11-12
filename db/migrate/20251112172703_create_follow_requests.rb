54a18769d341200d179a30be884d577c15a61d33
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-11-12 10:29:29 -0700

    feat: update migration with constraints & index

--- db/migrate/20251112172703_create_follow_requests.rb
+++ db/migrate/20251112172703_create_follow_requests.rb
@@ -1,11 +1,13 @@
 class CreateFollowRequests < ActiveRecord::Migration[8.0]
   def change
     create_table :follow_requests do |t|
-      t.references :follower, null: false, foreign_key: true
-      t.references :followed, null: false, foreign_key: true
-      t.string :status
+      t.references :follower, null: false, foreign_key: { to_table: :users }
+      t.references :followed, null: false, foreign_key: { to_table: :users }
+      t.string :status, default: 'pending', null: false
 
       t.timestamps
     end
+
+    add_index :follow_requests, [:follower_id, :followed_id], unique: true
   end
 end
