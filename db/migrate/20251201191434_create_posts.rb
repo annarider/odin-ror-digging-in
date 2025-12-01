318d1a992a555aded3625c6470b0e8b18aba741c
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-12-01 12:19:15 -0700

    feat: add validation to Post model on text content

--- db/migrate/20251201191434_create_posts.rb
+++ db/migrate/20251201191434_create_posts.rb
@@ -1,7 +1,7 @@
 class CreatePosts < ActiveRecord::Migration[8.0]
   def change
     create_table :posts do |t|
-      t.text :content
+      t.text :content, null: false
       t.string :image
       t.references :user, null: false, foreign_key: true
 
