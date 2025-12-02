274054378b555d94040e7abd6b858bfeed161a56
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-12-02 10:47:15 -0700

    feat: add Posts controller with RESTful actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,51 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :edit, :destroy]
+
   def index
+    @posts = current_user.posts
   end
 
   def show
+    @post = current_user.posts.find(params[:id])
   end
 
   def new
+    @post = current_user.posts.build
   end
 
   def edit
+    @post = current_user.posts.find(params[:id])
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+
+    if @post.save
+      redirect_to post_path(@post), notice: 'New post created'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @post = current_user.posts.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to posts_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @post = current_user.posts.find(params[:id])
+    @post.destroy
+    redirect_to posts_path, notice: "Post deleted"
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content, :image)
   end
 end
