7ed260b9086d46a9a249d92ade86d388587c91e4
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-12-03 17:15:04 -0700

    feat: add Comments controller actions

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,40 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_post
   def create
-  end
+    @comment = @post.comments.build(comment_params)
+    @comment.user = current_user
 
-  def edit
+    if @comment.save
+      redirect_to @post, notice: "Comment posted."
+    else
+      render "posts/show", status: :unprocessable_entity
+    end
   end
 
   def update
+    @comment = Comment.find(params[:id])
+
+    if @comment.update(comment_params)
+      redirect_to @post
+    else
+      render "posts/show", status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    redirect_to @post, status: :see_other, notice: "Comment removed."
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:post_id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:comment)
   end
 end
