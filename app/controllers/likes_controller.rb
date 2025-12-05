610cc2691d66f9b87779eec3a79d0f770de27796
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-12-05 13:51:11 -0700

    feat: populate Likes controller actions

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,7 +1,34 @@
 class LikesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_likeable, only: :create
+
   def create
+    # Build a new like associated with the current user and the likeable (Post or Comment)
+    @like = @likeable.likes.build(user: current_user)
+
+    if @like.save
+      redirect_to posts_path, notice: "Successfully liked!"
+    else
+      redirect_to posts_path, alert: "Couldn't add like."
+    end
   end
 
   def destroy
+    # Find the like that belongs to the current user
+    @like = current_user.likes.find(params[:id])
+    @like.destroy
+    redirect_to posts_path, notice: "Like removed."
+  end
+
+  private
+
+  # This method handles the polymorphic routing from nested routes
+  # It determines whether we're liking a Post or a Comment
+  def set_likeable
+    if params[:post_id]
+      @likeable = Post.find(params[:post_id])
+    elsif params[:comment_id]
+      @likeable = Comment.find(params[:comment_id])
+    end
   end
 end
