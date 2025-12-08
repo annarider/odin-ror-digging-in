febbf8e6e076b921c49f4f4ce5b75260c374006a
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-12-08 13:35:10 -0700

    test: nested routes & polymorphic associations

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -1,23 +1,51 @@
 require "test_helper"
 
 class CommentsControllerTest < ActionDispatch::IntegrationTest
-  test "should get create" do
-    get comments_create_url
-    assert_response :success
+  setup do
+    @user = users(:one)
+    @other_user = users(:two)
+    @post = posts(:one)
+    sign_in @user
   end
 
-  test "should get edit" do
-    get comments_edit_url
-    assert_response :success
+  # Create action tests
+  test "should create comment on post" do
+    assert_difference("Comment.count", 1) do
+      post post_comments_path(@post), params: { comment: { content: "Great gardening tip!" } }
+    end
+    assert_redirected_to post_path(@post)
+    assert_equal "Comment posted.", flash[:notice]
   end
 
-  test "should get update" do
-    get comments_update_url
-    assert_response :success
+  test "should not create comment without content" do
+    assert_no_difference("Comment.count") do
+      post post_comments_path(@post), params: { comment: { content: "" } }
+    end
+    assert_response :unprocessable_entity
   end
 
-  test "should get destroy" do
-    get comments_destroy_url
-    assert_response :success
+  test "created comment should belong to current user" do
+    post post_comments_path(@post), params: { comment: { content: "Test comment" } }
+    assert_equal @user.id, Comment.last.user_id
+  end
+
+  # Destroy action tests
+  test "should destroy own comment" do
+    comment = @post.comments.create(content: "My comment", user: @user)
+
+    assert_difference("Comment.count", -1) do
+      delete comment_path(comment)
+    end
+    assert_redirected_to post_path(@post)
+    assert_equal "Comment removed.", flash[:notice]
+  end
+
+  test "should not allow destroying other user's comment" do
+    comment = @post.comments.create(content: "Other's comment", user: @other_user)
+
+    assert_no_difference("Comment.count") do
+      delete comment_path(comment)
+      assert_response :not_found
+    end
   end
 end
--- test/controllers/likes_controller_test.rb
+++ test/controllers/likes_controller_test.rb
@@ -1,13 +1,59 @@
 require "test_helper"
 
 class LikesControllerTest < ActionDispatch::IntegrationTest
-  test "should get create" do
-    get likes_create_url
-    assert_response :success
+  setup do
+    @user = users(:one)
+    @other_user = users(:two)
+    @post = posts(:one)
+    sign_in @user
   end
 
-  test "should get destroy" do
-    get likes_destroy_url
-    assert_response :success
+  # Create action tests
+  test "should create like on post" do
+    assert_difference("Like.count", 1) do
+      post post_likes_path(@post)
+    end
+    assert_redirected_to posts_path
+    assert_equal "Successfully liked!", flash[:notice]
+  end
+
+  test "should create like on comment" do
+    comment = @post.comments.create(content: "Test comment", user: @other_user)
+
+    assert_difference("Like.count", 1) do
+      post comment_likes_path(comment)
+    end
+    assert_redirected_to posts_path
+    assert_equal "Successfully liked!", flash[:notice]
+  end
+
+  test "should not create duplicate like on same post" do
+    @post.likes.create(user: @user)
+
+    assert_no_difference("Like.count") do
+      post post_likes_path(@post)
+    end
+    assert_redirected_to posts_path
+    assert_equal "Couldn't add like.", flash[:alert]
+  end
+
+  # Destroy action tests
+  test "should destroy own like" do
+    like = @post.likes.create(user: @user)
+
+    assert_difference("Like.count", -1) do
+      delete like_path(like)
+    end
+    assert_redirected_to posts_path
+    assert_equal "Like removed.", flash[:notice]
+  end
+
+  test "should not allow destroying other user's like" do
+    like = @post.likes.create(user: @other_user)
+
+    assert_no_difference("Like.count") do
+      delete like_path(like)
+      assert_response :not_found
+    end
   end
 end
