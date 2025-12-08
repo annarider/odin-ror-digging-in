795cc45aa2ebe067c1c408cc2ae2423825ce79ca
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-12-08 13:52:16 -0700

    feat: add Gravatar app helper for using in views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  # Generates a Gravatar URL for a given user
+  # Gravatar uses MD5 hash of email to generate profile pictures
+  # Options:
+  #   size: pixel size of the image (default: 80)
+  def gravatar_url(user, size: 80)
+    # Gravatar requires lowercase, trimmed email hashed with MD5
+    gravatar_id = Digest::MD5.hexdigest(user.email.downcase.strip)
+
+    # Use 'identicon' as default image style if user has no Gravatar
+    # Other options: 'mp', 'monsterid', 'wavatar', 'retro', 'robohash'
+    "https://www.gravatar.com/avatar/#{gravatar_id}?s=#{size}&d=identicon"
+  end
 end
