b2e004a27930600c382f2143b54871d07cbed662
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-12-08 14:00:32 -0700

    test: add test for Gravatar app helper methods

---
+++ test/helpers/application_helper_test.rb
@@ -0,0 +1,40 @@
+require "test_helper"
+
+class ApplicationHelperTest < ActionView::TestCase
+  test "gravatar_url returns a valid Gravatar URL" do
+    # Arrange - use existing fixture
+    user = users(:one)
+
+    # Act
+    url = gravatar_url(user)
+
+    # Assert - Should generate a displayable Gravatar URL
+    assert url.start_with?("https://www.gravatar.com/avatar/")
+    assert url.include?("s=80") # default size
+    assert url.include?("d=identicon") # default image style
+  end
+
+  test "gravatar_url respects size option" do
+    # Arrange - use existing fixture
+    user = users(:one)
+
+    # Act
+    url = gravatar_url(user, size: 150)
+
+    # Assert
+    assert url.include?("s=150")
+  end
+
+  test "gravatar_url generates different URLs for different users" do
+    # Arrange - use existing fixtures
+    user1 = users(:one)
+    user2 = users(:two)
+
+    # Act
+    url1 = gravatar_url(user1)
+    url2 = gravatar_url(user2)
+
+    # Assert - Different users should have different profile pictures
+    assert_not_equal url1, url2
+  end
+end
