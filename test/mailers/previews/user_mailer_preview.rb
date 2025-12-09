0b9bb2165d5ba437c02ac684309fe71eb2a3642e
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-12-09 14:42:36 -0700

    test: Mailer test should use sample user

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -2,6 +2,11 @@
 class UserMailerPreview < ActionMailer::Preview
   # Preview this email at http://localhost:3000/rails/mailers/user_mailer/welcome_email
   def welcome_email
-    UserMailer.welcome_email
+    # Create a sample user for preview purposes (not saved to database)
+    user = User.new(
+      name: "Jane Gardener",
+      email: "jane@example.com"
+    )
+    UserMailer.welcome_email(user)
   end
 end
