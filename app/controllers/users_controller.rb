cee2337ae9c0e44d72bd95297343549af3df523b
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-12-09 10:08:31 -0700

    feat: add Users#index action to display all users
    - use Users#index action to show all users except
    for self
    - display all users to allow sending friend requests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,12 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
 
+  def index
+    # Load all users except the current user
+    # We exclude current_user since you can't send a friend request to yourself
+    @users = User.where.not(id: current_user.id).order(:name)
+  end
+
   def show
     @user = User.find(params[:id])
     # Load the user's posts ordered by most recent first
