b5209551d8d9f8cc7b1dc5a9f9bf3842e52b2cfb
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-12-16 12:57:35 -0700

    feat: install active storage & add association

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,6 +16,9 @@ class User < ApplicationRecord
   # Likes association
   has_many :likes, dependent: :destroy
 
+  # Active Storage association for profile photo
+  has_one_attached :avatar
+
   # Friend requests this user sent - get FriendRequest Objects
   has_many :sent_requests, foreign_key: :sender_id, class_name: "FriendRequest", dependent: :destroy
 
